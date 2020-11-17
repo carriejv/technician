b4765ceb302c3d39b4ac58a28b4eaa77020a1dc6
Carrie J V <carrie@carriejv.com>
2020-11-16 16:54:32 -0800

    Update index exports.

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import {
 } from './technician/technician';
 
 import {
+    TechnicianSync
+} from './technician/technician-sync';
+
+import {
     DefaultInterpreters
 } from './interpreters/default-interpreters';
 
@@ -10,8 +14,20 @@ import {
     DefaultInterpretersSync
 } from './interpreters/default-interpreters-sync';
 
+import {
+    ConfigSource,
+    ConfigSourceSync,
+    Interpreter,
+    InterpreterSync
+} from './types/source-types';
+
 export {
     Technician,
+    TechnicianSync,
     DefaultInterpreters,
-    DefaultInterpretersSync
+    DefaultInterpretersSync,
+    ConfigSource,
+    ConfigSourceSync,
+    Interpreter,
+    InterpreterSync
 };
